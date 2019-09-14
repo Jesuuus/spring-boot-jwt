65d19123773ba92fe7f51af23d915ea4d468d26f
Jesuuus <jesusvallejo@hotmail.es>
2019-09-14 19:18:36 +0200

    Seccion 12 video 147

--- src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
+++ src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
@@ -5,10 +5,17 @@ package com.bolsadeideas.springboot.app;
 //import org.slf4j.Logger;
 //import org.slf4j.LoggerFactory;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -31,6 +38,23 @@ public class MvcConfig implements WebMvcConfigurer {
 	public void addViewControllers(ViewControllerRegistry registry){
 		registry.addViewController("/error_403").setViewName("error_403");
 	}
-	
 
+	@Bean//resuelve el locale donde se va a guardar el parametro de nuestro lenguaje y sesion
+	public LocaleResolver localeResolver(){
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(new Locale("es","ES"));
+		return localeResolver;
+	}
+
+	@Bean//Es el interceptor que se encarga de modificar y cambiar el lenguaje al pasar el parametro lang por url
+	public LocaleChangeInterceptor localeChangeInterceptor(){
+		LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
+		localeInterceptor.setParamName("lang");
+		return localeInterceptor;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
+	}
 }
